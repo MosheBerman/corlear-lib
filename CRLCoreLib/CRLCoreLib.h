e4ec4dfd48a73872a2ceb3637e3cdbc45a066a53
Moshe <moshe@Moshes-MacBook-Air.local>
2014-06-25 09:24:17 -0400

    Initial Commit

---
+++ CRLCoreLib/CRLCoreLib.h
@@ -0,0 +1,13 @@
+//
+//  CRLCoreLib.h
+//  CRLCoreLib
+//
+//  Created by Moshe on 6/25/14.
+//  Copyright (c) 2014 Corlear Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRLCoreLib : NSObject
+
+@end
---
+++ CRLCoreLib/CRLCoreLib.m
@@ -0,0 +1,13 @@
+//
+//  CRLCoreLib.m
+//  CRLCoreLib
+//
+//  Created by Moshe on 6/25/14.
+//  Copyright (c) 2014 Corlear Apps. All rights reserved.
+//
+
+#import "CRLCoreLib.h"
+
+@implementation CRLCoreLib
+
+@end
